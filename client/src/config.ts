580b077d3d16311e4df7ca804b6a2d64d63f93e8
prubayita <prubayita@gmail.com>
2022-08-08 05:37:20 +0200

    all

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '48o5qq5g15';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-fjp-3wf9.us.auth0.com',
+  clientId: 'RzGQEDmJF4A5OznOSj7zIYBQ1zWTe1dT',
+  callbackUrl: 'http://localhost:3000/callback'
+};
